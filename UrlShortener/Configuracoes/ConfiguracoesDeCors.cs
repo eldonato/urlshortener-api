7e349fbefe39f82ec3ed1fdfb1f65d8ded2f9bcc
leonardodonato <kqjut3@gmail.com>
2025-05-11 23:35:03 -0300

    feat: adicionando URL do render como origin permitida do CORS

--- UrlShortener/Configuracoes/ConfiguracoesDeCors.cs
+++ UrlShortener/Configuracoes/ConfiguracoesDeCors.cs
@@ -9,7 +9,7 @@ public static class ConfiguracoesDeCors
             options.AddDefaultPolicy(builder =>
             {
                 builder
-                    .WithOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+                    .WithOrigins("http://localhost:3000", "http://127.0.0.1:3000", "https://urlshortener-app-a7f8.onrender.com")
                     .AllowAnyHeader()
                     .AllowAnyMethod();
             });
